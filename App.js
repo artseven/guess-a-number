c11b4e68516ecfa81d3333921ff6dfaf20a0298b
artseven <yabkevich.artsiom.09@gmail.com>
2019-09-04 16:30:19 -0400

    adding start game screen

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen'
 
 export default function App() {
   return (
-    <View >
-      
+    <View style={styles.screen}>
+      <Header title="Guess A Number" />
+      <StartGameScreen />
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  screen: {
+    flex: 1
+  }
 });
