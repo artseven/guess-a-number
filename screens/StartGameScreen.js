89c9e3a3e12e54556a05ad1e4ec4fe55ee5a69f0
artseven <yabkevich.artsiom.09@gmail.com>
2019-09-05 10:59:50 -0400

    component for numbers

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -12,6 +12,7 @@ import {
 import Card from '../components/Card';
 import Colors from '../constants/colors';
 import Input from '../components/Input';
+import NumberContainer from '../components/NumberContainer';
 
 const StartGameScreen = props => {
 
@@ -41,12 +42,19 @@ const StartGameScreen = props => {
 		setConfirmed(true);
 		setSelectedNumber(parseInt(enteredValue));
 		setEnteredValue('');
+		Keyboard.dismiss();
 	}
 
 	let confirmedOutput;
 
 	if (confirmed) {
-		confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>
+		confirmedOutput = (
+			<Card style={styles.summaryContainer}>
+				<Text>You selected</Text>
+				<NumberContainer>{selectedNumber}</NumberContainer>
+				<Button title="START GAME"/>
+			</Card>
+		);
 	};
 
 	return (
@@ -109,6 +117,10 @@ const styles = StyleSheet.create({
 	input: {
 		width: 50,
 		textAlign: 'center'
+	},
+	summaryContainer: {
+		marginTop: 20,
+		alignItems: 'center'
 	}
 });
 
