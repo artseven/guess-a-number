174e6be303d4303b60e8662aadfda3213124c652
artseven <yabkevich.artsiom.09@gmail.com>
2019-09-05 10:17:26 -0400

    adding handlers for both buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Text, Button, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { 
+	View, 
+	StyleSheet, 
+	Text, 
+	Button, 
+	TouchableWithoutFeedback, 
+	Keyboard 
+} from 'react-native';
 
 import Card from '../components/Card';
 import Colors from '../constants/colors';
@@ -8,9 +15,32 @@ import Input from '../components/Input';
 const StartGameScreen = props => {
 
 	const [enteredValue, setEnteredValue] = useState('');
+	const [confirmed, setConfirmed] = useState(false);
+	const [selectedNumber, setSelectedNumber] = useState();
 
 	const numberInputHandler = inputText => {
 		setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+	};
+
+	const resetInputHandler = () => {
+		setEnteredValue('');
+		setConfirmed(false);
+	}
+
+	const confirmInputHandler = () => {
+		const chosenNumber = parseInt(enteredValue);
+		if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+			return;
+		}
+		setConfirmed(true);
+		setSelectedNumber(parseInt(enteredValue));
+		setEnteredValue('');
+	}
+
+	let confirmedOutput;
+
+	if (confirmed) {
+		confirmedOutput = <Text>Chose Number: {selectedNumber}</Text>
 	};
 
 	return (
@@ -33,13 +63,14 @@ const StartGameScreen = props => {
 					/>
 					<View style={styles.buttonContainer}>
 						<View style={styles.button}>
-							<Button title="Reset" onPress={() => {}} color={Colors.secondary} />
+							<Button title="Reset" onPress={resetInputHandler} color={Colors.secondary} />
 						</View>
 						<View style={styles.button}>
-							<Button title="Confirm" onPress={() => {}} color={Colors.primary}/>
+							<Button title="Confirm" onPress={confirmInputHandler} color={Colors.primary}/>
 						</View>
 					</View>
 				</Card>
+				{confirmedOutput}
 			</View>
 		</TouchableWithoutFeedback>
 	);
