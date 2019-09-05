c054772da53474e6565f5b1b3baed136f68838b7
artseven <yabkevich.artsiom.09@gmail.com>
2019-09-05 10:37:51 -0400

    adding alert for entering restricted values

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,8 @@ import {
 	Text, 
 	Button, 
 	TouchableWithoutFeedback, 
-	Keyboard 
+	Keyboard,
+	Alert 
 } from 'react-native';
 
 import Card from '../components/Card';
@@ -29,7 +30,12 @@ const StartGameScreen = props => {
 
 	const confirmInputHandler = () => {
 		const chosenNumber = parseInt(enteredValue);
-		if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+		if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+			Alert.alert(
+				'Invalid number!', 
+				'Number has to be a number between 1 and 99.', 
+				[{text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+			);
 			return;
 		}
 		setConfirmed(true);
@@ -40,7 +46,7 @@ const StartGameScreen = props => {
 	let confirmedOutput;
 
 	if (confirmed) {
-		confirmedOutput = <Text>Chose Number: {selectedNumber}</Text>
+		confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>
 	};
 
 	return (
