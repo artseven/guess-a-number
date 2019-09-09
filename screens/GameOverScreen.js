6aa7b2a8e6127987f6f0fdf6ed09a6a58febfcdb
artseven <yabkevich.artsiom.09@gmail.com>
2019-09-09 17:23:42 -0400

    game over screen styling finished

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, Button, Image } from 'react-native';
 
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
+import Colors from '../constants/colors';
 
 const GameOverScreen = props => {
 	return (
@@ -10,13 +11,18 @@ const GameOverScreen = props => {
 			<TitleText>The Game is Over!</TitleText>
 			<View style={styles.imageWrapper}>
 				<Image 
-					source={require('../assets/success.png')} 
+					// source={require('../assets/success.png')} 
+					source={{uri: 'https://blog.strava.com/wp-content/uploads/2018/06/DSC02332-1.jpg'}} 
 					style={styles.image} 
 					// resizeMode="contain"
 				/>
 			</View>
-			<BodyText>Number of rounds: {props.roundsNumber}</BodyText>
-			<BodyText>Number was: {props.userNumber}</BodyText>
+			<View style={styles.resultContainer}>
+				<BodyText style={styles.resultText}>
+					Your phone needed <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to guess 
+					the number <Text style={styles.highlight}>{props.userNumber}</Text>.
+				</BodyText>
+			</View>
 			<Button title="NEW GAME" onPress={props.onRestart} />
 		</View>
 	);
@@ -40,6 +46,18 @@ const styles = StyleSheet.create({
 		borderColor: 'black',
 		overflow: "hidden",
 		marginVertical: 20
+	},
+	resultContainer: {
+		marginHorizontal: 30,
+		marginVertical: 15
+	},
+	highlight: {
+		color: Colors.primary,
+		fontFamily: 'open-sans-bold',
+	},
+	resultText: {
+		textAlign: 'center',
+		fontSize: 20
 	}
 });
 
