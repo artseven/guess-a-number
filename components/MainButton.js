c09ea536e77980f78563fbbbed7b85aa6336d4a3
artseven <yabkevich.artsiom.09@gmail.com>
2019-09-13 10:17:37 -0400

    adding platform specific native feel & look for buttons

--- components/MainButton.js
+++ components/MainButton.js
@@ -1,28 +1,47 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import Colors from '../constants/colors';
-
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+	TouchableNativeFeedback,
+	Platform
+} from "react-native";
+import Colors from "../constants/colors";
 
 const MainButton = props => {
-	return <TouchableOpacity onPress={props.onPress}>
-		<View style={styles.button}>
-			<Text style={styles.buttonText}>{props.children}</Text>
-		</View>
-	</TouchableOpacity>
+  let ButtomComponent = TouchableOpacity;
+
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    ButtomComponent = TouchableNativeFeedback;
+  }
+  return (
+    <View style={styles.buttonContainer}> 
+      <ButtomComponent onPress={props.onPress}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>{props.children}</Text>
+        </View>
+      </ButtomComponent>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	button: {
-		backgroundColor: Colors.primary,
-		paddingVertical: 12,
-		paddingHorizontal: 30,
-		borderRadius: 25
+	buttonContainer: {
+		borderRadius: 25,
+		overflow: 'hidden'
 	},
-	buttonText: {
-		color: 'white',
-		fontFamily: 'open-sans',
-		fontSize: 18
-	}
+  button: {
+    backgroundColor: Colors.primary,
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 25
+  },
+  buttonText: {
+    color: "white",
+    fontFamily: "open-sans",
+    fontSize: 18
+  }
 });
 
 export default MainButton;
