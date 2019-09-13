8a76842c19c9ee0e9a3317c720bba799dd3f20f8
artseven <yabkevich.artsiom.09@gmail.com>
2019-09-13 11:44:36 -0400

    wrapping up, app is finished

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -6,7 +6,8 @@ import {
   Button,
   Image,
   Dimensions,
-  ScrollView
+  ScrollView,
+  SafeAreaView
 } from "react-native";
 
 import BodyText from "../components/BodyText";
@@ -16,28 +17,30 @@ import MainButton from "../components/MainButton";
 
 const GameOverScreen = props => {
   return (
-    <ScrollView>
-      <View style={styles.screen}>
-        <TitleText>The Game is Over!</TitleText>
-        <View style={styles.imageWrapper}>
-          <Image
-            source={require("../assets/summit.jpg")}
-            // source={{uri: 'https://blog.strava.com/wp-content/uploads/2018/06/DSC02332-1.jpg'}}
-            style={styles.image}
-            // resizeMode="contain"
-          />
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.screen}>
+          <TitleText>The Game is Over!</TitleText>
+          <View style={styles.imageWrapper}>
+            <Image
+              source={require("../assets/summit.jpg")}
+              // source={{uri: 'https://blog.strava.com/wp-content/uploads/2018/06/DSC02332-1.jpg'}}
+              style={styles.image}
+              // resizeMode="contain"
+            />
+          </View>
+          <View style={styles.resultContainer}>
+            <BodyText style={styles.resultText}>
+              Your phone needed{" "}
+              <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds
+              to guess the number{" "}
+              <Text style={styles.highlight}>{props.userNumber}</Text>.
+            </BodyText>
+          </View>
+          <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
         </View>
-        <View style={styles.resultContainer}>
-          <BodyText style={styles.resultText}>
-            Your phone needed{" "}
-            <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
-            guess the number{" "}
-            <Text style={styles.highlight}>{props.userNumber}</Text>.
-          </BodyText>
-        </View>
-        <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
@@ -46,8 +49,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-		backgroundColor: "#ccc",
-		height: Dimensions.get('window').height
+    backgroundColor: "#ccc",
+    height: Dimensions.get("window").height
   },
   image: {
     width: "100%",
